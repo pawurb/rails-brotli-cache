f03577df374f1879c0baa5334fd8dc2337385c82
pawurb <p.urbanek89@gmail.com>
2023-05-16 16:08:23 +0200

    Initial dummy app

--- spec/rails-brotli-cache/main_spec.rb
+++ spec/rails-brotli-cache/main_spec.rb
@@ -2,10 +2,21 @@
 
 require 'spec_helper'
 
-describe RailsBrotliCache::Main do
-  describe "it" do
-    it "works" do
-      expect(true).to be true
+describe RailsBrotliCache do
+  context "without prepending Rails.cache" do
+    describe "#fetch" do
+      it "stores value in the configured Rails.cache" do
+        puts Rails.cache.class
+      end
     end
+
+    describe "#read" do
+
+    end
+
+    describe "#write" do
+
+    end
+
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'rails'
+
 require_relative '../lib/rails-brotli-cache'
+require_relative '../spec/dummy/config/environment'
+ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
 
 RSpec.configure do |config|
 end
